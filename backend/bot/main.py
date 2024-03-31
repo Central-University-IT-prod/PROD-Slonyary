ccb1adb700027e8d2d1979347e9a1e57eb806414
ZENT <zzentq@gmail.com>
2024-03-31 15:27:46 +0300

    Точка входа в бота

--- backend/bot/main.py
+++ backend/bot/main.py
@@ -1,1 +1,20 @@
-print('hello world')
+import asyncio
+
+from pyrogram import Client
+from pyrogram.types import (ReplyKeyboardMarkup,
+                            InlineKeyboardMarkup,
+                            InlineKeyboardButton)
+
+from core.settings.config import TOKEN
+
+# Create a client using your bot token
+app = Client("my_bot", bot_token=TOKEN)
+
+
+async def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
