cbf884e46b9e9a570bade674123cd4c43c6179f6
StreveSuksess <strevesuksess@gmail.com>
2024-04-01 01:55:45 +0300

    change post item model

--- frontend/src/models/PostsModels.ts
+++ frontend/src/models/PostsModels.ts
@@ -1,11 +1,12 @@
 export type TPostItem = {
-	channelAvatar: string,
-	channelName: string,
-	date: string,
-	publicised: boolean,
-	admin: string,
-	postImages: string[],
-	postText: string,
-	category: 'pending' | 'moderation' | 'published',
-	ownerName: string,
+  channelAvatar: string,
+  channelName: string,
+  date: string,
+  publicised: boolean,
+  admin: string,
+  postImages: string[],
+  category: 'pending' | 'moderation' | 'published',
+  ownerName: string,
+  htmlText?: string,
+  plainText?: string
 }
