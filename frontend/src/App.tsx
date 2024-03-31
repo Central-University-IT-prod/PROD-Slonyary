985464bdb9cd573111fb0dd1715754f8b3cfbba9
Dima Kozlov <zibocinfinity@gmail.com>
2024-03-31 10:51:50 +0300

    comit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import { useMemo } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import { ThemeProvider, createTheme } from '@mui/material'
 import useAppSelector from './hooks/useAppSelector'
-=======
-import { useMemo } from "react";
-import { Outlet } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material";
-import useAppSelector from "./hooks/useAppSelector";
->>>>>>> 75fe88f2dbaf78729dd6d6972cb59d4db6f34119
 
 function App() {
 	const { mode: themeMode } = useAppSelector((state) => state.theme)
