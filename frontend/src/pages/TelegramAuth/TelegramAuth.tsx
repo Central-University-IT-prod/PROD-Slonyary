878454a762abfabf703a9597279779c852d510f4
Dima Kozlov <zibocinfinity@gmail.com>
2024-04-01 21:35:18 +0300

    auth fix 3

--- frontend/src/pages/TelegramAuth/TelegramAuth.tsx
+++ frontend/src/pages/TelegramAuth/TelegramAuth.tsx
@@ -12,13 +12,7 @@ export const TelegramAuth: FC = () => {
 			setUser(response)
 
 			const res = await axios.post(`http://${BACKEND_HOST}/auth`, {
-				id: 860014279,
-				first_name: 'Стас',
-				username: 'strevesuksess',
-				photo_url:
-					'https://t.me/i/userpic/320/7YeNB5xPCbYNEzk96vXhuwDUpjVeifoP3JxF0zF9MWg.jpg',
-				auth_date: 1711962110,
-				hash: '6b1fe21be7236d58c69b3211a6bf03a7a7be3fb7393238cad4650a68c8ed3813'
+				...response
 			})
 			console.log(res)
 			if (res) {
