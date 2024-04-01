1a4423cfbbb4e35ef1bba893f42b7d583e769f03
K1rLes <kirill.lesovoi@mail.ru>
2024-04-01 15:54:01 +0300

    Интеграция

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,9 @@
-from app.api.routes import auth, channel, post
 from fastapi import APIRouter
+
+from app.api.routes import auth, channel, ping, post
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(post.router, prefix="/posts", tags=["post"])
 api_router.include_router(channel.router, prefix="/channels", tags=["channel"])
+api_router.include_router(ping.router, tags=["ping"])
