f21443633d75d30f2d6f7fc00b527d6c66f40d0c
K1rLes <kirill.lesovoi@mail.ru>
2024-04-01 15:16:17 +0300

    Удалил ненужные файлы

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const THEME_KEY: string = 'appTheme'
-export const BACKEND_HOST = 'prodapi.zzentqgpt.ru'
+export const THEME_KEY: string = "appTheme"
+export const BACKEND_HOST = "prodapi.zzentqgpt.ru"
