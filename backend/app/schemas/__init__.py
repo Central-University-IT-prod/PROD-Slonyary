1a7ecc9b1b11b0e7577d790216f7ae77fcc839c6
K1rLes <kirill.lesovoi@mail.ru>
2024-04-01 21:03:29 +0300

    result

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,7 +1,8 @@
 from .image import ImageCreate, ImageRead, ImageUpdate
-from .post import PostCreate, PostIn, PostRead, PostUpdate, PreviewPost
+from .post import Channel, PostCreate, PostIn, PostRead, PostUpdate, PreviewPost
 from .posts_to_tg import PostsToTgChannelsCreate
 from .posts_to_vk import PostsToVkChannelsCreate
+from .result import Result
 from .tg_channel import (
     PreviewTgChannel,
     TgChannelCreate,
@@ -11,10 +12,16 @@ from .tg_channel import (
 )
 from .user import UserCreate, UserRead, UserTelegramData, UserUpdate
 from .users_to_tg_channels import UsersToTgChannelsCreate
-from .vk_channel import VkChannelCreate, VkChannelRead, VkChannelUpdate
 from .users_to_vk_channels import UsersToVkChannelsCreate
+from .vk_channel import VkChannelCreate, VkChannelRead, VkChannelUpdate
 
 __all__ = (
+    "PostsToVkChannelsCreate",
+    "PostsToTgChannelsCreate",
+    "UsersToVkChannelsCreate",
+    "UsersToTgChannelsCreate",
+    "UsersToVkChannelsCreate",
+    "PostIn",
     "PreviewTgChannel",
     "TgChannelMember",
     "TgChannelMember",
@@ -35,4 +42,6 @@ __all__ = (
     "ImageCreate",
     "ImageUpdate",
     "ImageRead",
+    "Result",
+    "Channel",
 )
