5fa13eb8207215088c0587411d95e5f0f32efe4a
Dima Kozlov <zibocinfinity@gmail.com>
2024-04-01 15:26:33 +0300

    bug fix

--- frontend/src/appRouter.tsx
+++ frontend/src/appRouter.tsx
@@ -2,10 +2,10 @@ import { createBrowserRouter } from 'react-router-dom'
 import { paths } from './routes'
 import App from './App'
 import RegistrationPage from './pages/RegistrationPage'
-import HomePage from '../../../HomePage.tsx'
 import PostPage from './pages/PostPage/PostPage.tsx'
 import { TelegramAuth } from './pages/TelegramAuth/TelegramAuth.tsx'
 import { ChannelPage } from './pages/ChannelPage/ChannelPage.tsx'
+import HomePage from './pages/HomePage.tsx'
 
 export const router = createBrowserRouter([
 	{
