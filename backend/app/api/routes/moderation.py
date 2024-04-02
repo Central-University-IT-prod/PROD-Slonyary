c83567c6077c9e7061ed481512de9baea5bd8825
K1rLes <kirill.lesovoi@mail.ru>
2024-04-02 19:22:10 +0300

    publish пост сразу

--- backend/app/api/routes/moderation.py
+++ backend/app/api/routes/moderation.py
@@ -3,7 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from starlette import status
 
-from app.api.deps.moderation import accept_post_dep, downgrade_post_dep, reject_post_dep
+from app.api.deps.moderation import (
+    accept_post_dep,
+    downgrade_post_dep,
+    publish_post_dep,
+    reject_post_dep,
+)
 from app.schemas import Result
 from shared.database.models import Post
 
@@ -20,6 +25,11 @@ async def downgrade_post(_: Annotated[Post, Depends(downgrade_post_dep)]) -> Res
     return Result(status="ok")
 
 
-@router.delete("/reject", status_code=200)
+@router.delete("/reject", status_code=status.HTTP_200_OK)
 async def reject_post(_: Annotated[Post, Depends(reject_post_dep)]) -> Result:
+    return Result(status="ok")
+
+
+@router.post("/publish", status_code=status.HTTP_200_OK)
+async def publish_post(_: Annotated[Post, Depends(publish_post_dep)]) -> Result:
     return Result(status="ok")
