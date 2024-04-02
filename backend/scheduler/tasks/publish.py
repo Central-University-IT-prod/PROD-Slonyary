30785bfc28cd0f89810bd6378d9eaf8e868aa0a3
K1rLes <kirill.lesovoi@mail.ru>
2024-04-02 22:54:10 +0300

    логи в шедулер

--- backend/scheduler/tasks/publish.py
+++ backend/scheduler/tasks/publish.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from aiogram import Bot
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -24,7 +25,10 @@ async def post_publisher(
             await publish_tg_post(post, bot, session)
             # await publish_vk_post(post)
 
+            logging.info(f"Отправил пост {post.id}")
             await mark_post_as_published(post, session)
+            logging.info(f"Отметил пост {post.id} опубликованным")
             await notify_owner_about_publish(post, bot)
+            logging.info(f"Сообщил создателю поста {post.id} о публикации")
 
         await asyncio.sleep(interval)
